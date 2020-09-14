bd08cc921227a9b37411406b78f8c407b55e174e
Grigore Stefan <g_stefan@yahoo.com>
2020-09-14 17:41:25 +0300

    update

--- port/build.wsl.sh
+++ port/build.wsl.sh
@@ -4,7 +4,7 @@
 # Created by Grigore Stefan <g_stefan@yahoo.com>
 
 # On WSL we must build on home folder to keep executable bits on files
-WSL_BUILD_PROCESS_PATH=$HOME/SDK/xyo
+WSL_BUILD_PROCESS_PATH=$HOME/SDK/xyo-version
 export XYO_PATH_REPOSITORY=$HOME/SDK/Repository
 export XYO_PATH_RELEASE=$HOME/SDK/Release
 
