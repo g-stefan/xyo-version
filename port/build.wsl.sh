6cd68bb747634a04c8e13ad8319727693cea71b6
Grigore Stefan <g_stefan@yahoo.com>
2020-12-02 15:15:57 +0200

    update

--- port/build.wsl.sh
+++ port/build.wsl.sh
@@ -14,9 +14,11 @@ export XYO_PATH_REPOSITORY_LIBRARY=
 
 [ -d $WSL_BUILD_PROCESS_PATH/source ] || mkdir -p $WSL_BUILD_PROCESS_PATH/source
 [ -d $WSL_BUILD_PROCESS_PATH/port ] || mkdir -p $WSL_BUILD_PROCESS_PATH/port
+[ -d $WSL_BUILD_PROCESS_PATH/release ] || mkdir -p $WSL_BUILD_PROCESS_PATH/release
 
 cp -rfu ./source/* $WSL_BUILD_PROCESS_PATH/source/
 cp -rfu ./port/build.ubuntu*.sh $WSL_BUILD_PROCESS_PATH/port/
+cp -rfu ./release/*.csv $WSL_BUILD_PROCESS_PATH/release/
 
 WSL_POPD=$PWD
 cd $WSL_BUILD_PROCESS_PATH
@@ -35,10 +37,7 @@ fi
 if [ "$1" = "local-release" ]; then
 	[ -d ./release ] || mkdir -p ./release 
 	cp -rfu $WSL_BUILD_PROCESS_PATH/release/*.7z ./release/
-	for file in $WSL_BUILD_PROCESS_PATH/release/*.csv; do
-		cat $file >> ./release/$(basename "$file")
-		break
-	done
+	cp -rfu $WSL_BUILD_PROCESS_PATH/release/*.csv ./release/
 fi
 
 if [ "$RETV" = "1" ]; then
