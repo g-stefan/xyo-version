b92426c681ec755de3c87ef16696b679ea14c1fa
Grigore Stefan <g_stefan@yahoo.com>
2021-08-09 15:40:45 +0300

    Update

--- source/xyo-version-version.cpp
+++ source/xyo-version-version.cpp
@@ -13,9 +13,9 @@ namespace XYOVersion {
 	namespace Version {
 
 		static const char *version_ = "3.2.0";
-		static const char *build_ = "15";
-		static const char *versionWithBuild_ = "3.2.0.15";
-		static const char *datetime_ = "2021-07-23 16:34:00";
+		static const char *build_ = "16";
+		static const char *versionWithBuild_ = "3.2.0.16";
+		static const char *datetime_ = "2021-08-05 20:38:13";
 
 		const char *version() {
 			return version_;
