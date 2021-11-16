d0ce450c6dd1c2daff3eea9fda561d408972ef53
Grigore Stefan <g_stefan@yahoo.com>
2021-11-17 01:40:12 +0200

    Update

--- build/ubuntu.release.sh
+++ build/ubuntu.release.sh
@@ -14,7 +14,7 @@ cmdX(){
 
 export XYO_PATH_RELEASE=release
 
-if xyo-cc xyo-version --has-archived-release --version-file=version.ini; then
+if ! xyo-cc xyo-version --has-archived-release --version-file=version.ini; then
 	exit 0
 fi
 cmdX /bin/sh ./build/ubuntu.sh make
