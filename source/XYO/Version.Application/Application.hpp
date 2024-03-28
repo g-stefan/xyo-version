464effcf9130902da70cfc71c841b512fcbc06fb
Grigore Stefan <g_stefan@yahoo.com>
2024-03-28 12:14:25 +0200

    Update

--- source/XYO/Version.Application/Application.hpp
+++ source/XYO/Version.Application/Application.hpp
@@ -14,7 +14,7 @@
 namespace XYO::Version::Application {
 
 	class Application : public virtual IApplication {
-			XYO_DISALLOW_COPY_ASSIGN_MOVE(Application);
+			XYO_PLATFORM_DISALLOW_COPY_ASSIGN_MOVE(Application);
 
 		public:
 			inline Application(){};
